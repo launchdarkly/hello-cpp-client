b9ca435d0644c5603af7eb6b10267f478407f21e
Elliot <35050275+Apache-HB@users.noreply.github.com>
2020-08-20 13:41:17 -0400

    update readme and modify hello.cpp with more documentation

--- hello.cpp
+++ hello.cpp
@@ -2,16 +2,20 @@
 
 const char *username = "jeff@example.com";
 
+// update me to your own keys
+const char *sdk_key = "YOUR_SDK_KEY";
+const char *feature_key = "YOUR_FEATURE_KEY";
+
 int main() {
     LDConfigureGlobalLogger(LD_LOG_INFO, LDBasicLogger);
 
-    auto *config = LDConfigNew("YOUR_SDK_KEY");
+    auto *config = LDConfigNew(sdk_key);
     auto *user = LDUserNew(username);
 
     // wait up to 3 seconds to connect
     auto *client = LDClientCPP::Init(config, user, 3000);
 
-    auto feature = client->boolVariation("YOUR_FEATURE_KEY", false);
+    auto feature = client->boolVariation(feature_key, false);
 
     if (feature) {
         printf("My feature was enabled for %s\n", username);

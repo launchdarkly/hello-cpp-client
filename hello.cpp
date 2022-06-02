656ea6749566b27846b854ce04431d94d8683c5e
Casey Waldren <cwaldren@launchdarkly.com>
2022-06-02 14:25:11 -0700

    Update to use latest hello app patterns (#6)
    * Inject mobile & feature keys via CircleCI environment variable
    
    * Update README with latest hello-app language
    
    * Verify expected flag value
    
    * Allow M1 builds to succeed using the x86_64 artifacts

--- hello.cpp
+++ hello.cpp
@@ -1,28 +1,46 @@
+#include <cstring>
 #include <launchdarkly/api.hpp>
 
-const char *username = "jeff@example.com";
+// Set MOBILE_KEY to your LaunchDarkly mobile key.
+#define MOBILE_KEY ""
 
-// update me to your own keys
-const char *sdk_key = "YOUR_SDK_KEY";
-const char *feature_key = "YOUR_FEATURE_KEY";
+// Set FEATURE_KEY to the feature flag you want to evaluate.
+#define FEATURE_KEY "YOUR_FEATURE_KEY"
 
 int main() {
+
+    if (strlen(MOBILE_KEY) == 0) {
+        printf("*** Please edit hello.cpp to set MOBILE_KEY to your LaunchDarkly mobile key first\n\n");
+        return 1;
+    }
+
     LDConfigureGlobalLogger(LD_LOG_INFO, LDBasicLogger);
 
-    auto *config = LDConfigNew(sdk_key);
-    auto *user = LDUserNew(username);
+    auto *config = LDConfigNew(MOBILE_KEY);
+
+    // Set up the user properties. This user should appear on your LaunchDarkly users dashboard
+    // soon after you run the demo.
+    auto *user = LDUserNew("example-user-key");
+    LDUserSetName(user, "Sandy");
 
     // wait up to 3 seconds to connect
     auto *client = LDClientCPP::Init(config, user, 3000);
 
-    auto feature = client->boolVariation(feature_key, false);
-
-    if (feature) {
-        printf("My feature was enabled for %s\n", username);
+    if (client->isInitialized()) {
+        printf("*** SDK successfully initialized!\n\n");
     } else {
-        printf("My feature was not enabled for %s\n", username);
+        printf("*** SDK failed to initialize\n\n");
+        return 1;
     }
 
-    // cleanup after ourselves
+    auto flag_value = client->boolVariation(FEATURE_KEY, false);
+
+    printf("*** Feature flag '%s' is %s for this user\n\n", FEATURE_KEY, flag_value ? "true" : "false");
+
+    // Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+    // events to LaunchDarkly before the program exits. If analytics events are not delivered,
+    // the user properties and flag usage statistics will not appear on your dashboard. In a
+    // normal long-running application, the SDK would continue running and events would be
+    // delivered automatically in the background.
     client->close();
 }

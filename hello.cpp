04ffc2a5c84dc6f73a00bd16ee391e0d15ec8aae
Casey Waldren <cwaldren@launchdarkly.com>
2022-06-02 11:18:43 -0700

    Inject mobile & feature keys via environment variable

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,19 @@
+#include <cstring>
 #include <launchdarkly/api.hpp>
 
 const char *username = "jeff@example.com";
 
 // update me to your own keys
-const char *sdk_key = "YOUR_SDK_KEY";
-const char *feature_key = "YOUR_FEATURE_KEY";
+#define MOBILE_KEY "YOUR_MOBILE_KEY"
+#define FEATURE_KEY "YOUR_FEATURE_KEY"
 
 int main() {
+
+    if (strlen(MOBILE_KEY) == 0) {
+        printf("*** Please edit hello.cpp to set MOBILE_KEY to your LaunchDarkly mobile key first\n\n");
+        return 1;
+    }
+
     LDConfigureGlobalLogger(LD_LOG_INFO, LDBasicLogger);
 
     auto *config = LDConfigNew(sdk_key);
